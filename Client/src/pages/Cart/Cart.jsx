151a2b53bd693a20c6d9bde78a112299f6e89c25
WeBstwr <ifedhawebster@gmail.com>
2024-07-03 21:52:07 +0300

    create cart layout
    I have created a basic layout for the cart page

--- Client/src/pages/Cart/Cart.jsx
+++ Client/src/pages/Cart/Cart.jsx
@@ -1,5 +1,33 @@
+import "./cart.css";
+import Banner from "../../components/Banner/Banner";
+
 const Cart = () => {
-  return <></>;
+  return (
+    <>
+      <section className="cart">
+        <div className="cart-bg">
+          <h2>my cart</h2>
+        </div>
+        <Banner mainText="my-items" />
+        <div className="cart-items">
+          <h3>no items yet</h3>
+          {/* <div className="cart-item">
+            <div className="cart-item-img">
+              image
+              <div className="cart-item-text">
+                <h3>product name</h3>
+                <p>product description</p>
+              </div>
+            </div>
+            <button>$$</button>
+          </div> */}
+        </div>
+        <div className="item-price">
+          <button>continue shopping</button>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Cart;
