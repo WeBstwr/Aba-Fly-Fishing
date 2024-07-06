e50778f071872b6718986ac6c375aa3d2d199730
WeBstwr <ifedhawebster@gmail.com>
2024-07-06 14:27:44 +0300

    create user model
    I have created the user model

---
+++ Server/controllers/users.controllers.js
@@ -0,0 +1,22 @@
+import bcrypt from "bcrypt";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export const createUser = async (req, res) => {
+  try {
+    const { userName, firstName, lastName, email, password } = req.body;
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    const newUser = await prisma.user.create({
+      data: {
+        userName: userName,
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: hashedPassword,
+      },
+    });
+    res.status(200).json(newUser);
+  } catch (event) {
+    res.status(500).json({ success: false, message: "Internal Error" });
+  }
+};
--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,12 @@
 import express from "express";
+import usersRouter from "./routes/users.routes.js";
 import { config } from "dotenv";
 config();
 
 const app = express();
+app.use(express.json());
+
+app.use("/api/users", usersRouter);
 
 app.listen(8080, () => {
   console.log("Server is running on port 8080");
