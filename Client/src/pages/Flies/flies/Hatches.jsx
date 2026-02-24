fa80c2267963a4ab7edb210b8719fb339e00f173
WeBstwr <ifedhawebster@gmail.com>
2026-02-24 13:14:05 +0300

    update packages
    -security updates

--- Client/src/pages/Flies/flies/Hatches.jsx
+++ Client/src/pages/Flies/flies/Hatches.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import FliesList from "../FliesList.jsx";
+import hatchesData from "../../../data/hatchesData.js"
 
-function Hatches() {
-  return <div>Hatches</div>;
-}
+const Hatches = () => {
+  const navigate = useNavigate();
+
+  const handleClick = (fly) => {
+    navigate("/Purchase", {
+      state: { fly },
+    });
+  };
+
+  return (
+    <section className="flies">
+      <h4>Hatches</h4>
+      <FliesList fliesData={hatchesData} handleClick={handleClick} />
+    </section>
+  );
+};
 
 export default Hatches;
